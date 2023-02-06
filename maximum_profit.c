e030fdf724b5ce2f461d35f8931cad9ee41e4c5d
Deekshamanoj <109511259+Deekshamanoj@users.noreply.github.com>
2023-02-06 15:12:37 +0530

    Update maximum_profit.c

--- maximum_profit.c
+++ maximum_profit.c
@@ -4,12 +4,14 @@
 
 #define max_jobs 100
 
+//structure to store the start_time, end_time and profit
 struct job{
     int start_time;
     int end_time;
     int profit;
 };
 
+//function to compare the profit of two jobs
 int compare(const void *a, const void *b){
     struct job *j1 = a;
     struct job *j2 = b;
@@ -18,18 +20,23 @@ int compare(const void *a, const void *b){
 
 int main(){
     int n;
+    //asking user to enter the no of jobs
     printf("Enter the number of Jobs: ");
     scanf("%d", &n);
 
     struct job jobs[n];
+    //asking user to enter details of different jobs
     printf("Enter job start time, end time, and earnings:\n");
     for(int i=0; i<n; i++){
         scanf("%d%d%d", &jobs[i].start_time, &jobs[i].end_time, &jobs[i].profit);
     }
 
+//sorting the sort the profits which are compared in compare function
     qsort(jobs, n, sizeof(struct job), compare);
 
+/* variables to store the no of task(count), earnings for other members and to store end time of current work */
     int count=0, earnings=0, end_time=0;
+    // calculating the no of task and earnings for rest of members
     for(int i=0; i<n; i++){
         if(jobs[i].start_time >= end_time){
             count++;
@@ -41,6 +48,7 @@ int main(){
         }
     }
 
+// pronting the final output
     printf("The number of tasks and earnings available for others\nTask: %d\nEarnings: %d\n", n-count, earnings);
 
     return 0;
